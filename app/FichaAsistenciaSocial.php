f59c1fb8f72c7c93bf1723553e7985e52c58ec68
gabrielmc85 <gabrielcampagna@hotmail.com>
2018-02-14 20:29:43 -0300

    fichaAsistenciaSocial models Completos

--- app/FichaAsistenciaSocial.php
+++ app/FichaAsistenciaSocial.php
@@ -4,7 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class FichaAsistenciaSocial extends Model
+class FichaAsistenciaSocial extends Ficha
 {
-    //
+    protected $table="fichasAsistenciasSociales";
+    protected $fillable = [
+        'checkAsistenciaSocial',
+        'servicios',
+       
+    ];
+
+    public function serviciosSociales(){
+        return $this->hasMany('App\ServicioSocial');
+    }
+
+    public function asistido(){
+        return $this->belongsTo('App\Asistido');
+    }
 }
