6b03f4401dd90f52eaf302129be487c6dafff0ff
gabrielmc85 <gabrielcampagna@hotmail.com>
2018-04-03 19:31:36 -0300

    FichaDiagnosticoIntegral model

---
+++ app/CursoDeAccion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CursoDeAccion extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table="cursosDeAccion";
+    protected $fillable = [
+        'meta','fechaDesde','fechaHasta','accion',
+    ];
+
+   	public function fichaDiagnosticoIntegral(){
+   		return $this->belongsTo('App\FichaDiagnosticoIntegral');
+   	}
+}
---
+++ app/FichaDiagnosticoIntegral.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FichaDiagnosticoIntegral extends Ficha
+{
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table="fichasDiagnosticosIntegrales";
+    protected $fillable = [
+        'diagnostico','trabajoInterdisciplinario',
+    ];
+
+   	public function cursosDeAccion(){
+   		return $this->HasMany('App\CursoDeAccion');
+   	}
+
+   	public function asistido(){
+        return $this->belongsTo('App\Asistido');
+    }
+}
