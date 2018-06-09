f470f437cec97bda71a3ca63ae671ddf6afd136b
nicoviola91 <nico_viola91@hotmail.com>
2018-06-08 21:25:16 -0300

    UsuariosCompartidos
    Crea la tabla UsuariosCompartidos

--- app/UsuarioCompartido.php
+++ app/UsuarioCompartido.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsuarioCompartido extends Model
 {
-    //
+    protected $table="usuariosCompartidos";
+    protected $fillable=[
+    		'fichaDatosPersonales_id',
+    		'fichaAdicciones_id',
+    		'fichaAsistenciaSocial_id',
+    		'fichaDiagnosticoIntegral_id',
+    		'fichaEducacion_id',
+    		'fichaEmpleo_id',
+    		'fichaFamiliaAmigos_id',
+    		'fichaLegal_id',
+    		'fichaLocalizacion_id',
+    		'fichaMedica_id',
+    		'fichaNecesidad_id',
+    		'fichaSaludMental_id',
+    		'user_id',
+    	];
+    
+    public function user(){
+        return $this->belongsTo('App\Users');
+    }
+    public function ficha(){
+    	return $this->belongsTo('App\Ficha');
+    }
+    /*public function asistido(){
+    	return $this->belongsTo('App\Asistido');
+    }*/
 }
