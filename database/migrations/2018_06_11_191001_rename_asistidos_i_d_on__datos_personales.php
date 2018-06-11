36cfa3ca53c6ca4125e01e9bbb32745c622ae66f
nicoviola91 <nico_viola91@hotmail.com>
2018-06-11 19:22:26 -0300

    Fichas Datos Personales Rename Asistidos_id
    Renomba la columna asistidos_id por asistido_id para que lo pueda levantar el controlador. Y borra una migration que no va.

--- database/migrations/2018_06_11_191001_rename_asistidos_i_d_on__datos_personales.php
+++ database/migrations/2018_06_11_191001_rename_asistidos_i_d_on__datos_personales.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFichaDatosPersonales extends Migration
+class RenameAsistidosIDOnDatosPersonales extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateFichaDatosPersonales extends Migration
      */
     public function up()
     {
-        Schema::create('fichasDatosPersonales', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        Schema::table('fichasDatosPersonales', function (Blueprint $table) {
+            $table->renameColumn('asistidos_id', 'asistido_id');
         });
     }
 
@@ -26,6 +25,8 @@ class CreateFichaDatosPersonales extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fichasDatosPersonales');
+        Schema::table('fichasDatosPersonales', function (Blueprint $table) {
+            //
+        });
     }
 }
