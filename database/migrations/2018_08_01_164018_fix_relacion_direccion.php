9318382c952d9dc08d00616ed5cd67f1f9ddc688
juanagustingallo <juan.agustin.gallo@gmail.com>
2018-08-01 13:43:10 -0300

    mgiraciones para arreglar la relacion

---
+++ database/migrations/2018_08_01_164018_fix_relacion_direccion.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixRelacionDireccion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instituciones', function (Blueprint $table) {
+            //
+            $table->dropColumn('direccion');
+
+            $table->integer('direccion_id')->unsigned();
+            $table->foreign('direccion_id')->references('id')->on('direcciones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instituciones', function (Blueprint $table) {
+            //
+        });
+    }
+}
