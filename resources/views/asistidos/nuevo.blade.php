4ee88a61839779f0eb17ad89c7f471dd5eda0f3c
juanagustingallo <juan.agustin.gallo@gmail.com>
2018-08-08 12:26:36 -0300

    ..

--- resources/views/asistidos/nuevo.blade.php
+++ resources/views/asistidos/nuevo.blade.php
@@ -12,7 +12,6 @@
 	<small>Nuevo Asistido</small>
 </h1>
 <br>
-<p>Atención! Si el asistido tiene alertas generadas, es conveniente dar de alta el asistido partiendo de su alerta, para un mejor seguimiento del caso </p>
 <ol class="breadcrumb">
 	<li><a href="#"><i class="fa fa-bell"></i> Asistidos</a></li>
 	<li class="active">Nuevo</li>
@@ -22,24 +21,25 @@
 @section('content')
 
 <div class="row">
+
+<div class="alert alert-info alert-dismissible col-md-8 col-md-offset-2" style="margin-bottom: 8px;">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h4><i class="icon fa fa-info"></i> Atención!</h4>
+   Si el asistido tiene alertas generadas es conveniente dar de alta el asistido desde de su alerta, para un mejor seguimiento del caso.
+   <br>Hacé <a href="{{url('alert/list')}}">CLICK ACÁ</a> para ver las Alertas pendientes
+</div>
+
 <div class="col-md-10 col-md-offset-1">
-	<div class="box box-solid">
+	
+  <h3>Paso 1 <small>INGRESÁ LOS DATOS DE LA PERSONA</small></h3>
+  <div class="box box-solid">
 		
 		<div class="box-body">
 			
 			<form role="form" id="nuevoAsistido-form" method="POST" action="{{ url('/asistido/storeNew') }}">
         {{ csrf_field() }}
         <div class="box-body">
-            <div class="form-group">
-              @if(isset($comunidades))
-              <label for="comunidad">Comunidad al que pertenece como usuario</label>
-              <select class="form-control" name="comunidad" id="comunidad" >
-                  @foreach($comunidades as $comunidad)
-                    <option value="{{$comunidad->id}}">{{$comunidad->nombre}}</option>
-                  @endforeach
-              </select>
-          </div>
-            @endif
+            
           <div class="form-group">
             <label for="exampleInputEmail1">Nombre</label>
             <input type="text" class="form-control" id="name" placeholder='Nombre' name="nombre"  maxlength="250" required >
