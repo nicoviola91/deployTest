c6cb4d9977afb0404c2d456f6310261390287c0c
juanagustingallo <juan.agustin.gallo@gmail.com>
2018-09-06 21:20:02 -0300

    asisitido usuario-creador

--- app/Http/Controllers/AsistidoController.php
+++ app/Http/Controllers/AsistidoController.php
@@ -112,6 +112,7 @@ class AsistidoController extends Controller
         
         $asistido->createdBy=Auth::user()->id; //Usuario que creo el asistido
         $asistido->owner = $alerta->user_id; //Usuario que creo la alerta
+        $asistido->user = Auth::user()->id;
 
         $asistido->save();
         
@@ -152,6 +153,7 @@ class AsistidoController extends Controller
         
         $asistido->createdBy=Auth::user()->id;
         $asistido->owner = Auth::user()->id;
+        $asistido->user = Auth::user()->id;
 
         if (isset($request->comunidad_id)) {
             
