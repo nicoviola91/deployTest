296c8f09071b12a6f6103912d7515714776fec99
Nico Viola <nico_viola91@hotmail.com>
2018-09-17 23:52:12 -0300

    Correccion Mis Alertas
    Correccion

--- resources/views/alertas/misAlertas.blade.php
+++ resources/views/alertas/misAlertas.blade.php
@@ -210,15 +210,15 @@
 								<td class="vert-aligned text-center" style="vertical-align: middle;">{{ isset($alerta->institucion->nombre) ? $alerta->institucion->nombre : '' }}</td>
 								
 								<td class="vert-aligned text-center">
-									<?php if ($alerta->estado = 1) { ?>
+									<?php if ($alerta->estado == 1) { ?>
 										
 										<span class="label label-success"><i class="fa icon fa-check-circle"></i> SE PRESENTÓ </span>
 
-									<?php } elseif ($alerta->estado = 0) { ?>
+									<?php } elseif ($alerta->estado == 0) { ?>
 
 										<span class="label label-default"><i class="fa icon fa-clock-o"></i> PENDIENTE </span>
 									
-									<?php } elseif ($alerta->estado = 2) { ?>
+									<?php } elseif ($alerta->estado == 2) { ?>
 
 										<span class="label label-danger"><i class="fa icon fa-clock-o"></i> DESCARTADA </span>
 
