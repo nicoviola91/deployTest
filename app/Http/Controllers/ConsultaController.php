fe332d5241c7892173f93fe0693be842dbca501e
juanagustingallo <juan.agustin.gallo@gmail.com>
2018-09-20 20:06:30 -0300

    fix consulta notificacion

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -139,12 +139,12 @@ class ConsultaController extends Controller
             $consulta->adjunto = $path;
         }
 
-        if ($ficha->created_by != $consulta->user_id){
-            $asistido_notif = Asistido::where('id',$asistido_id)->get()->first();
+        // if ($ficha->created_by != $consulta->user_id){
+        //     $asistido_notif = Asistido::where('id',$asistido_id)->get()->first();
 
-            $usuarioNotif = User::where('id',$ficha->created_by)->get()->first();
-            $usuarioNotif->notify(new NuevaConsulta($consulta, $asistido_notif));//, $tipo_notif));
-        }
+        //     $usuarioNotif = User::where('id',$ficha->created_by)->get()->first();
+        //     $usuarioNotif->notify(new NuevaConsulta($consulta, $asistido_notif));//, $tipo_notif));
+        // }
     
         if ($ficha->consultas()->save($consulta)) {
             
